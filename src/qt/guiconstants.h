35017451a6e8f3ff65c6c5159cf291402abf9e9c
Felix The Cat <meow@felixcoin.org>
2021-06-01 11:23:59 +0300

    fix qt path

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -48,7 +48,7 @@ static const int MAX_URI_LENGTH = 255;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 36
 
-#define QAPP_ORG_NAME "Felixcoin"
+#define QAPP_ORG_NAME "Felixcoinclassic"
 #define QAPP_ORG_DOMAIN "felixcoin.org"
 #define QAPP_APP_NAME_DEFAULT "Felixcoin-Qt"
 #define QAPP_APP_NAME_TESTNET "Felixcoin-Qt-testnet"
